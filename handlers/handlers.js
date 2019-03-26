cdb4960caff8e84fe463c2bffbe31a816d5d7d53
Ankit chawla <ankit.chawla@cybergindia.local>
2019-03-26 21:11:48 +0530

    more structural changes

---
+++ handlers/handlers.js
@@ -0,0 +1,60 @@
+
+const LaunchRequestHandler = {
+
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+      return handlerInput.responseBuilder.speak('Welcome To Hack The Machine Team 2')
+        .reprompt('Sorry, I can\'t understand the command. Please say again.')
+        .getResponse();
+    },
+  };
+  
+  const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+          || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+      const speechText = 'Goodbye!';
+  
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .withSimpleCard('Hello World', speechText)
+        .getResponse();
+    },
+  };
+  
+  const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+      console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+  
+      return handlerInput.responseBuilder.getResponse();
+    },
+  };
+  
+  const ErrorHandler = {
+    canHandle() {
+      return true;
+    },
+    handle(handlerInput, error) {
+      console.log(`Error handled: ${error.message}`);
+  
+      return handlerInput.responseBuilder
+        .speak('Sorry, I can\'t understand the command. Please say again.')
+        .reprompt('Sorry, I can\'t understand the command. Please say again.')
+        .getResponse();
+    },
+  };
+  
+module.exports = {
+    LaunchRequestHandler,
+    CancelAndStopIntentHandler, 
+    SessionEndedRequestHandler,
+    ErrorHandler
+}
--- index.js
+++ index.js
@@ -1,66 +1,11 @@
 'use strict';
 
 const Alexa = require('ask-sdk-core');
-
+const {LaunchRequestHandler, CancelAndStopIntentHandler, SessionEndedRequestHandler, ErrorHandler}
+= require('./handlers/handlers')
 if ('undefined' === typeof process.env.DEBUG) {
   Alexa.appId = '...';
-  var AWS = require('aws-sdk');
-  var dyn = new AWS.DynamoDB({ endpoint: new AWS.Endpoint("http://localhost:8000") });
-  Alexa.dynamoDBClient = dyn;
 }
-
-const LaunchRequestHandler = {
-
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder.speak('Welcome To Hack The Machine Team 2')
-      .reprompt('Sorry, I can\'t understand the command. Please say again.')
-      .getResponse();
-  },
-};
-
-const CancelAndStopIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
-  },
-  handle(handlerInput) {
-    const speechText = 'Goodbye!';
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
-  },
-};
-
-const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-  },
-  handle(handlerInput) {
-    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
-
-    return handlerInput.responseBuilder.getResponse();
-  },
-};
-
-const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${error.message}`);
-
-    return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Please say again.')
-      .reprompt('Sorry, I can\'t understand the command. Please say again.')
-      .getResponse();
-  },
-};
 
 const skillBuilder = Alexa.SkillBuilders.custom();
 
@@ -72,4 +17,3 @@ exports.handler = skillBuilder
   )
   .addErrorHandlers(ErrorHandler)
   .lambda();
-
