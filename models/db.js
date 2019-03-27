bea320f9756b3fecc2a21e436fa8082f832792a6
Ankitchawla777 <ankitchawla777@gmail.com>
2019-03-27 22:22:55 +0530

    User authentication

--- models/db.js
+++ models/db.js
@@ -1,10 +1,12 @@
 "use-strict";
 
 let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/hack-the-machine',()=>{
-	console.log('mongodb connected to hack-the-machine database');
-})
+mongoose.promise = require('bluebird');
 
-module.exports = mongoose;
+mongoose.connect('mongodb://Admin:hack-team2@ds223756.mlab.com:23756/hack-the-machine',()=>{
+	console.log('mongodb connected to hack-the-machine database');
+}).catch((e) => console.log(e));
+
+module.exports = { mongoose };
 
 
